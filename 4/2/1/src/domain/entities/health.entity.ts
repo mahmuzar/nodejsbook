c4ea436ef431a65628073f2136773a36028bae9c
Murad Kadiev <mahmuzar@yandex.ru>
2026-01-21 02:24:09 +0300

    chapter 4.2.1 added tsconfig.build.json exclue test files ni tsconfig and fix small bug

--- 4/2/1/src/domain/entities/health.entity.ts
+++ 4/2/1/src/domain/entities/health.entity.ts
@@ -2,7 +2,7 @@ export class HealthEntity {
   readonly status: string;
   readonly timestamp: Date;
 
-  constructor( { status: string; timestamp: Date }) {
+  constructor( data: { status: string; timestamp: Date }) {
     this.status = data.status;
     this.timestamp = data.timestamp;
   }
